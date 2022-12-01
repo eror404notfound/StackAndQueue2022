6c0e483803fc8878affca67373019cc2cb850d8a
Ankit <ankitcool4370@gmail.com>
2022-12-01 20:59:55 +0530

    welcome to the stack and queue problem

---
+++ StackQueue2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue2022
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the stack element");
+            LinkedListStack stacklist = new LinkedListStack();
+            stacklist.Push(70);
+            stacklist.Push(30);
+            stacklist.Push(56);
+            stacklist.Display();
+            Console.ReadLine();
+        }
+    }
+}
